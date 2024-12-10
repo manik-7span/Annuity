ad9bc4974190e0ead30290812ebe627e89cb65aa
manik-7span <manik@7span.com>
2024-12-10 19:40:11 +0530

    full responsivenes

--- script/first.js
+++ script/first.js
@@ -11,17 +11,35 @@ document.getElementById('search-button').addEventListener('click', function () {
   });
   /*-burger icon-*/
   function toggleMenu() {
-    const navbarPoints = document.getElementById('hero__menu');
-    navbarPoints.classList.toggle('show');
+    const navInfo = document.querySelector('.hero__menu');
+    navInfo.classList.toggle('.hero__nav-list');
   }
+  
   $(document).ready(function(){
     $('.slider').slick({
-      dots: false, // Show dots for navigation
-      infinite: true, // Infinite looping
-      speed: 500, // Transition speed
-      slidesToShow: 3, // Number of slides to show
-      slidesToScroll: 2, // Number of slides to scroll
-      autoplay: true, // Enable autoplay
-      autoplaySpeed: 3000, // Autoplay speed
+        dots: false, // Show dots for navigation
+        infinite: true, // Infinite looping
+        speed: 500, // Transition speed
+        slidesToShow: 2.5, // Number of slides to show
+        slidesToScroll: 2, // Number of slides to scroll
+        autoplay: true, // Enable autoplay
+        autoplaySpeed: 3000, // Autoplay speed
+        responsive: [
+            {
+                breakpoint: 800, // Screen width at which the settings will change
+                settings: {
+                    slidesToShow: 2, // Number of slides to show at this breakpoint
+                    slidesToScroll: 1 // Number of slides to scroll at this breakpoint
+                }
+            },
+            {
+              breakpoint: 580, // Screen width at which the settings will change
+              settings: {
+                  slidesToShow: 1, // Number of slides to show at this breakpoint
+                  slidesToScroll: 1 // Number of slides to scroll at this breakpoint
+              }
+          }
+        ]
     });
-  });
+});
+
